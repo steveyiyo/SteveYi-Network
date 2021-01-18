09fe5def32c7a9e22dcc9ededda52c2faf63bff2
steveyiyo <jackcooku@steveyi.net>
2021-01-19 00:30:19 +0800

    Create BGP_filter.sh

---
+++ filter/bgp_filter.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+whois -h whois.radb.net '!i'"AS-STEVEYI-A" | sed '2!d; s/ /\n/g' | while read -r asn; do
+    echo "$asn"
+    v4_filter="$(bgpq3 -4l $asn"_v4" "$asn" -R 24)"
+    v6_filter="$(bgpq3 -6l $asn"_v6" "$asn" -R 48)"
+    asn_v4=${asn}_v4
+    asn_v6=${asn}_v6
+    vtysh <<EOF
+config
+$v4_filter
+$v6_filter
+route-map $asn_v4 permit 5
+match ipv6 address prefix-list $asn_v4
+set community 17413:400
+route-map $asn_v6 permit 5
+match ipv6 address prefix-list $asn_v6
+set community 17413:400
+exit
+exit
+write
+exit
+EOF
+
+done
