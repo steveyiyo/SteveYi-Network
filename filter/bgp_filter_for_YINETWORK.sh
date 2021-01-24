eee3e5a641bb9b05ad52f011df5a3de187d629ae
SteveYi <jackcooku@steveyi.net>
2021-01-24 23:30:30 +0800

    Create bgp_filter_for_YINETWORK.sh

---
+++ filter/bgp_filter_for_YINETWORK.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+whois -h whois.radb.net '!i'"AS-STEVEYI" | sed '2!d; s/ /\n/g' | while read -r asn; do
+    echo "$asn"
+    v4_filter="$(bgpq3 -4l $asn"_v4" "$asn" -R 24)"
+    v6_filter="$(bgpq3 -6l $asn"_v6" "$asn" -R 48)"
+    asn_v4=${asn}_v4
+    asn_v6=${asn}_v6
+    vtysh <<EOF
+config
+$v4_filter
+$v6_filter
+route-map CLEARNET permit 5
+match ip address prefix-list $asn_v4
+route-map CLEARNET permit 5
+match ipv6 address prefix-list $asn_v6
+exit
+exit
+write
+exit
+EOF
+
+done
